2adcb1f9799e4915859038b37c86b340a1771426
arielliv <ariell@wix.com>
2024-12-16 23:58:42 +0200

    step 04 - Refactor: board and controlled cells

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import './App.css';
 import {useState} from "react";
 
-const Cell = ({currentPlayerSign, toggleCurrentPlayerSign}) => {
-    const [val, setVal] = useState(null);
-
-    const onClickHandler = () => {
-        if (val === null) {
-            setVal(currentPlayerSign);
-            toggleCurrentPlayerSign();
-        }
-    }
-
-    return <td onClick={onClickHandler}>{val}</td>;
+const Cell = ({val, onCellClicked}) => {
+    return <td onClick={onCellClicked}>{val}</td>;
 };
 
-const Row = ({currentPlayerSign, toggleCurrentPlayerSign}) => (
+const Row = ({cells, onCellClicked, index}) => (
     <tr>
-        {[1, 2, 3].map((index) =>
+        {cells.map((val, cellIndex) =>
             <Cell
-                key={index}
-                currentPlayerSign={currentPlayerSign}
-                toggleCurrentPlayerSign={toggleCurrentPlayerSign}
+                key={cellIndex}
+                val={val}
+                onCellClicked={() => onCellClicked(index, cellIndex)}
             />
         )}
     </tr>
 );
 
+const initBoard = (n) => {
+    const board = [];
+    for (let i = 0; i < n; i++) {
+        board.push([]);
+        for (let j = 0; j < n; j++) {
+            board[i].push(null);
+        }
+    }
+
+    return board;
+}
+
+const cloneBoard = (board) => board.map(row => [...row]);
+
 function App() {
     const [currentPlayerSign, setCurrentPlayerSign] = useState('X');
+    const [board, setBoard] = useState(initBoard(3));
+
     const toggleCurrentPlayerSign = () =>
         currentPlayerSign === 'X' ? setCurrentPlayerSign('O') : setCurrentPlayerSign('X');
 
+    const onCellClicked = (rowIndex, cellIndex) => {
+        if (board[rowIndex][cellIndex] === null) {
+            const clonedBoard = cloneBoard(board);
+            clonedBoard[rowIndex][cellIndex] = currentPlayerSign;
+            setBoard(clonedBoard);
+            toggleCurrentPlayerSign();
+        }
+    }
 
     return (<div className="App">
         <header className="App-header">
@@ -38,11 +53,12 @@ function App() {
             <h2>Current player is: {currentPlayerSign}</h2>
             <table>
                 <tbody>
-                {[1, 2, 3].map((index) =>
+                {board.map((cells, index) =>
                     <Row
                         key={index}
-                        currentPlayerSign={currentPlayerSign}
-                        toggleCurrentPlayerSign={toggleCurrentPlayerSign}
+                        index={index}
+                        cells={cells}
+                        onCellClicked={onCellClicked}
                     />
                 )}
                 </tbody>
--- src/App.test.js
+++ src/App.test.js
@@ -1,39 +1,40 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders Tic Tac Toe heading', () => {
-    render(<App/>);
-    const [headingElement] = screen.getAllByRole('heading');
-    expect(headingElement).toHaveTextContent(/Tic Tac Toe/i);
+describe('App', () => {
+    test('renders Tic Tac Toe heading', () => {
+        render(<App/>);
+        const [headingElement] = screen.getAllByRole('heading');
+        expect(headingElement).toHaveTextContent(/Tic Tac Toe/i);
+    });
+
+    test('should render current player', () => {
+        render(<App/>);
+        const headingElement = screen.getByText(/current player/i);
+        expect(headingElement).toBeInTheDocument();
+    });
+
+    test('render Tic Tac Toe table', () => {
+        render(<App/>);
+        const tableElement = screen.getByRole('table');
+        expect(tableElement).toBeInTheDocument();
+        const rows = screen.getAllByRole('row');
+        expect(rows).toHaveLength(3);
+        const cells = screen.getAllByRole('cell');
+        expect(cells).toHaveLength(9);
+    });
+
+    test('cell click - should render current player', () => {
+        render(<App/>);
+        const [cell1, cell2] = screen.getAllByRole('cell');
+        expect(cell1).toBeEmptyDOMElement();
+        cell1.click();
+        expect(cell1).toHaveTextContent('X');
+        cell1.click();
+        expect(cell1).toHaveTextContent('X'); // should stay 'x'
+
+        expect(cell2).toBeEmptyDOMElement();
+        cell2.click();
+        expect(cell2).toHaveTextContent('O');
+    });
 });
-
-test('should render current player', () => {
-    render(<App/>);
-    const headingElement = screen.getByText(/current player/i);
-    expect(headingElement).toBeInTheDocument();
-});
-
-test('render Tic Tac Toe table', () => {
-    render(<App/>);
-    const tableElement = screen.getByRole('table');
-    expect(tableElement).toBeInTheDocument();
-    const rows = screen.getAllByRole('row');
-    expect(rows).toHaveLength(3);
-    const cells = screen.getAllByRole('cell');
-    expect(cells).toHaveLength(9);
-});
-
-test('cell click - should render current player', () => {
-    render(<App/>);
-    const [cell1, cell2] = screen.getAllByRole('cell');
-    expect(cell1).toBeEmptyDOMElement();
-    cell1.click();
-    expect(cell1).toHaveTextContent('X');
-    cell1.click();
-    expect(cell1).toHaveTextContent('X'); // should stay 'x'
-
-    expect(cell2).toBeEmptyDOMElement();
-    cell2.click();
-    expect(cell2).toHaveTextContent('O');
-})
-
