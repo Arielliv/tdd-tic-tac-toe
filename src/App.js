fb2ec4127f7286a12044851cd035e1b06d2b592e
arielliv <ariell@wix.com>
2024-12-16 18:09:33 +0200

    step 01 - hardcoded table and heading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Tic Tac Toe</h1>
+        <table>
+            <tbody>
+                <tr>
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                </tr>
+                <tr>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                </tr>
+                <tr>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                </tr>
+            </tbody>
+        </table>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Tic Tac Toe heading', () => {
+    render(<App/>);
+    const headingElement = screen.getByRole('heading');
+    expect(headingElement).toHaveTextContent(/Tic Tac Toe/i);
+});
+
+test('render Tic Tac Toe table', () => {
+    render(<App/>);
+    const tableElement = screen.getByRole('table');
+    expect(tableElement).toBeInTheDocument();
+    const rows = screen.getAllByRole('row');
+    expect(rows).toHaveLength(3);
+    const cells = screen.getAllByRole('cell');
+    expect(cells).toHaveLength(9);
 });
